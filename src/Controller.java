9d9ce0007bd010da11e24e4c06d524b7a014b4d6
Julian Sia <siajat@utexas.edu>
2015-10-01 20:05:22 -0500

    stub of controller

--- src/Controller.java
+++ src/Controller.java
@@ -1,8 +1,84 @@
+import java.util.HashMap;
+
 /**
  * Controller
  * @author Julian
  *
  */
 public class Controller {
+	NetController masterNetwork;
+	Config configFile;
+	Process[] idToProc; // i-th position corresponds to i-th process
+	Process currLeader;
+	Process lastKilled;
+	
+	
+	
+	public Controller() {
+		
+	}
+	
+	public void initiateAdd(String songName, String URL) {
+		
+	}
+	
+	public void initiateRemove(String songName) {
+		
+	}
+	
+	public void initiateEdit(String songName, String URL) {
+		
+	}
+	
+	public void createProcesses() {
+		
+	}
+	
+	public void kill(Integer id) {
+		
+	}
+	
+	public void killAll() {
+		
+	}
+	
+	public void killLeader() {
+		
+	}
+	
+	public void revive(Integer id) {
+		
+	}
+	
+	public void reviveLast(Integer id) {
+		
+	}
+	
+	public void reviveAll() {
+		
+	}
+	
+	public void partialMessage(Integer id, Integer numMsgs) {
+		
+	}
+	
+	public void resumeMessages(Integer id) {
+		
+	}
+	
+	public void allClear() {
+		
+	}
+	
+	public void rejectNextChange(Integer id) {
+		
+	}
+	
+	
+	public void runCommmands() {
+		
+	}
+
+	
 	
 }
