2ec1f8cecd1234ca562db2fdce7a6f8186671ca0
zmadmani <zain.admani@gmail.com>
2015-10-02 18:16:50 -0500

    First version with networking starting

--- src/Controller.java
+++ src/Controller.java
@@ -15,11 +15,22 @@ public class Controller {
 	Process currLeader;
 	Process lastKilled;
 	
-	
-	
-	public Controller() {
-		
-	}
+	public Controller() throws FileNotFoundException, IOException {
+		Config configP0 = new Config("properties_p0.txt");
+		NetController controlP0 = new NetController(configP0);
+		Config configP1 = new Config("properties_p1.txt");
+		NetController controlP1 = new NetController(configP1);
+		Config configP2 = new Config("properties_p2.txt");
+		NetController controlP2 = new NetController(configP2);
+		Config configP3 = new Config("properties_p3.txt");
+		NetController controlP3 = new NetController(configP3);
+		Config configP4 = new Config("properties_p4.txt");
+		NetController controlP4 = new NetController(configP4);
+		controlP0.shutdown();
+		controlP1.shutdown();
+		controlP2.shutdown();
+		controlP3.shutdown();
+		controlP4.shutdown();		}
 	
 	public void initiateAdd(String songName, String URL) {
 		
@@ -83,24 +94,7 @@ public class Controller {
 	}
 	
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		Config configP0 = new Config("properties_p0.txt");
-		NetController controlP0 = new NetController(configP0);
-		Config configP1 = new Config("properties_p1.txt");
-		NetController controlP1 = new NetController(configP1);
-		Config configP2 = new Config("properties_p2.txt");
-		NetController controlP2 = new NetController(configP2);
-		Config configP3 = new Config("properties_p3.txt");
-		NetController controlP3 = new NetController(configP3);
-		Config configP4 = new Config("properties_p4.txt");
-		NetController controlP4 = new NetController(configP4);
-//		System.out.println(InetAddress.getLocalHost());
-//		System.out.println(InetAddress.getLocalHost().getHostAddress());
-//		System.out.println(InetAddress.getByName("www.javacodegeeks.com"));
-//		InetAddress[] inetAddresses = InetAddress.getAllByName("www.google.com");
-//		for (InetAddress ipAddress : inetAddresses) {
-//			System.out.println(ipAddress);
-//		}
-
+		Controller mainController = new Controller();
 	}
 
 	
--- src/Process.java
+++ src/Process.java
@@ -6,17 +6,32 @@ import java.util.HashMap;
  * @author Zain Admani
  *
  */
-public class Process {
+public class Process extends Thread {
 	private NetController network;
 	private Integer id;
 	private Boolean amCoord;
 	private Boolean isTransactionOn;
+	private Boolean alive;
 	private Integer stage; //Tells which stage of the transaction it is on currently
 	private ArrayList<Integer> livingProcs;
 	private HashMap<String, String> playlist;
 	
-	public Process(Integer id) {
-		
+	public Process(Integer id, Config configFile) {
+		this.id = id;
+		network = new NetController(configFile);
+		alive = true;
+		amCoord = false;
+		isTransactionOn = false;
+		stage = 0; //0 --> nothing | 1 --> vote_req | 2 --> voted | 3 --> precommit | 4 --> ack | 5 --> commit | -1 = abort
+		playlist = new HashMap<String, String>();
+		//livingProcs
+	}
+	
+	public void run() {
+		int i = 0;
+		while(i < 5) {
+			System.out.println("I AM IN PROCESS " + i);
+		}
 	}
 	
 	private void sendVoteReq() {
